8de2c840589d9610f05194b54bc549cc649d4a4e
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-02-01 08:20:57 +0800

    Update routes src js

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,6 @@
 import React from 'react'
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
 import App from './App'
-
 import Create from './Create'
 
 const RoutedApp = () => {
@@ -11,11 +8,9 @@ const RoutedApp = () => {
     <Router>
       <Routes>
         <Route path="/" exact="true" element={<App />} />
-
         <Route path="/create" exact="true" element={<Create />} />
       </Routes>
     </Router>
   )
 }
-
 export default RoutedApp
